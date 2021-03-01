edca678fb99870f1ff2ca717b94a93d463429ca1
RubelBiswasCs <rubelbiswas36@gmail.com>
2021-03-01 23:01:50 +0600

    Added Dummy Data for blog on home page

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts =[
+    {
+    'author':'Jay',
+    'title':'Blog Post 1',
+    'content':'content of first post',
+    'date':'June 12, 2020'
+    },
+
+    {
+    'author':'Kay',
+    'title':'Blog Post 2',
+    'content':'content of second post',
+    'date' : 'January 10, 2021'
+    }
+]
+
 def home(request):
-    return render(request,'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About<h1/>')    
+    return render(request,'blog/about.html')   
 
