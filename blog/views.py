532a36572331b9208d3b3b7fd24ab97ae1712c08
RubelBiswasCs <rubelbiswas36@gmail.com>
2021-03-15 19:42:41 +0600

    Paginated home page

--- blog/views.py
+++ blog/views.py
@@ -22,6 +22,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
     context_object_name =  'posts'
     ordering = ['-date']
+    paginate_by = 5
 
 class PostDetailView(DetailView):
     model = Post
